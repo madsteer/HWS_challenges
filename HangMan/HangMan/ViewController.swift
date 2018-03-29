2ca58878bdf18a692c9ea59d44e05119c680cbc4
Cory Steers <developer@madsteer.com>
2018-03-28 21:35:04 -0500

    large parts of game wokring.  replacing "_" with a matched letter is not

--- HangMan/HangMan/ViewController.swift
+++ HangMan/HangMan/ViewController.swift
@@ -14,16 +14,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var guessedLettersLabel: UITextField!
 
     var guessedLetters = "" { didSet { guessedLettersLabel.text = guessedLetters } }
-    var wordToGuess = "rhythm"
-    var numberOfGuesses = 0
+    var wordToGuess = "RHYTHM"
+    var numberOfBadGuesses = 0 { didSet { hangManLabel.text = hangManGraphic[numberOfBadGuesses] } }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureLetterButtons()
 
-        hangManLabel.text = hangManGraphic[0]
+        hangManLabel.text = hangManGraphic[numberOfBadGuesses]
 
 //        guessLabel.text = "_ _ _ _ _ _"
         presentGuess()
+
 //        for hangMan in hangManGraphic {
 //            hangManLabel.text = hangMan
 
@@ -34,15 +36,45 @@ class ViewController: UIViewController {
 //        }
     }
 
-    private func presentGuess() {
-        let guessLength: Int = wordToGuess.count
-        var underscores = "_"
-
-        for _ in 1 ..< guessLength {
-            underscores += " _"
+    private func checkForMatching(pressed letter: String) {
+        if wordToGuess.contains(letter) {
+            var newUnderscores = ""
+            for letterToCheck in wordToGuess {
+                newUnderscores += (String(letterToCheck) == letter) ? letter : "_"
+            }
+            guessLabel.text = newUnderscores
+            guessLabel.addCharacterSpacing()
+        } else {
+            numberOfBadGuesses += 1
+            hangManLabel.text = hangManGraphic[numberOfBadGuesses]
         }
-        guessLabel.text = underscores
+
+        guessedLetters += letter
     }
 
+    private func presentGuess() {
+//        let guessLength: Int = wordToGuess.count
+        var underscores = ""
+
+        for _ in wordToGuess {
+            underscores += "_"
+        }
+        guessLabel.text = underscores
+        guessLabel.addCharacterSpacing()
+    }
+
+    private func configureLetterButtons() {
+        for subview in view.subviews where subview.tag == letterButtonTag {
+            let button = subview as! UIButton
+            button.addTarget(self, action: #selector(letterButtonPressed), for: .touchUpInside)
+        }
+    }
+
+    @objc private func letterButtonPressed(button: UIButton) {
+        if let letter = button.titleLabel?.text {
+            if guessedLetters.contains(letter) { return }
+            checkForMatching(pressed: letter)
+        }
+    }
 }
 
